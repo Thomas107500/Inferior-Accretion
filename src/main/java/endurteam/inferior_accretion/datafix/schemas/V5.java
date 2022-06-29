eafced5146cfa08ffbf4aa180c1aa09e415fb73a
Codetoil <ianthisawesomee@gmail.com>
2022-06-29 18:12:47 -0400

    Data fixers

---
+++ src/main/java/endurteam/inferior_accretion/datafix/schemas/V5.java
@@ -0,0 +1,16 @@
+package endurteam.inferior_accretion.datafix.schemas;
+
+import com.mojang.datafixers.schemas.Schema;
+import net.minecraft.util.datafix.schemas.NamespacedSchema;
+
+/**
+ * Corresponds with Inferior-Accretion 1.18.2-0.1.0+build.1
+ * @author Codetoil
+ */
+public class V5 extends NamespacedSchema
+{
+	public V5(int p_17308_, Schema p_17309_)
+	{
+		super(p_17308_, p_17309_);
+	}
+}
---
+++ src/main/java/endurteam/inferior_accretion/datafix/schemas/V6.java
@@ -0,0 +1,16 @@
+package endurteam.inferior_accretion.datafix.schemas;
+
+import com.mojang.datafixers.schemas.Schema;
+import net.minecraft.util.datafix.schemas.NamespacedSchema;
+
+/**
+ * Corresponds with Inferior-Accretion 1.19-0.1.0+build.0
+ * @author Codetoil
+ */
+public class V6 extends NamespacedSchema
+{
+	public V6(int p_17308_, Schema p_17309_)
+	{
+		super(p_17308_, p_17309_);
+	}
+}

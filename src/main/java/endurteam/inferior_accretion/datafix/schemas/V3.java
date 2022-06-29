7bed4ae69defff13ce1f4214b0a8e75f7028dc6b
Codetoil <ianthisawesomee@gmail.com>
2022-06-29 18:12:47 -0400

    Data fixers

---
+++ src/main/java/endurteam/inferior_accretion/datafix/schemas/V3.java
@@ -0,0 +1,16 @@
+package endurteam.inferior_accretion.datafix.schemas;
+
+import com.mojang.datafixers.schemas.Schema;
+import net.minecraft.util.datafix.schemas.NamespacedSchema;
+
+/**
+ * Corresponds with Habirance Ver-0.0.3-Alpha
+ * @author Codetoil
+ */
+public class V3 extends NamespacedSchema
+{
+	public V3(int p_17308_, Schema p_17309_)
+	{
+		super(p_17308_, p_17309_);
+	}
+}
---
+++ src/main/java/endurteam/inferior_accretion/datafix/schemas/V8.java
@@ -0,0 +1,16 @@
+package endurteam.inferior_accretion.datafix.schemas;
+
+import com.mojang.datafixers.schemas.Schema;
+import net.minecraft.util.datafix.schemas.NamespacedSchema;
+
+/**
+ * Corresponds with Inferior-Accretion 1.19-0.1.1+build.2
+ * @author Codetoil
+ */
+public class V8 extends NamespacedSchema
+{
+	public V8(int p_17308_, Schema p_17309_)
+	{
+		super(p_17308_, p_17309_);
+	}
+}

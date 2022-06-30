9eac075d84e44ad35bcc2d45f1f1891e1edd9772
Codetoil <ianthisawesomee@gmail.com>
2022-06-30 11:05:46 -0400

    Complete the change

--- src/main/java/endurteam/inferior_accretion/world/level/levelgen/feature/SimpleBlockWithBambooFeature.java
+++ src/main/java/endurteam/inferior_accretion/world/level/levelgen/feature/SimpleBlockWithBambooFeature.java
@@ -11,7 +11,6 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.DoublePlantBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BambooLeaves;
-import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 
@@ -30,14 +29,15 @@ public class SimpleBlockWithBambooFeature extends Feature<SimpleBlockWithBambooC
 	 * Places the given feature at the given location.
 	 * During world generation, features are provided with a 3x3 region of chunks, centered on the chunk being generated,
 	 * that they can safely generate into.
-	 * @param pContext A context object with a reference to the level and the position the feature is being placed at
+	 * @param context A context object with a reference to the level and the position the feature is being placed at
 	 */
-	public boolean place(FeaturePlaceContext<SimpleBlockWithBambooConfiguration> pContext) {
-		SimpleBlockWithBambooConfiguration configuration = pContext.config();
-		WorldGenLevel worldgenlevel = pContext.level();
-		BlockPos blockpos = pContext.origin();
-		BlockState blockstate = configuration.toPlace().getState(pContext.random(), blockpos);
-		if (blockstate.canSurvive(worldgenlevel, blockpos)) {
+	public boolean place(FeaturePlaceContext<SimpleBlockWithBambooConfiguration> context) {
+		SimpleBlockWithBambooConfiguration configuration = context.config();
+		WorldGenLevel worldgenlevel = context.level();
+		BlockPos blockpos = context.origin();
+		RandomSource randomsource = context.random();
+		BlockState blockstate = configuration.toPlace().getState(context.random(), blockpos);
+		if (blockstate.canSurvive(worldgenlevel, blockpos) && randomsource.nextFloat() < configuration.probability()) {
 			if (blockstate.getBlock() instanceof DoublePlantBlock) {
 				if (!worldgenlevel.isEmptyBlock(blockpos.above())) {
 					return false;
@@ -45,7 +45,7 @@ public class SimpleBlockWithBambooFeature extends Feature<SimpleBlockWithBambooC
 
 				DoublePlantBlock.placeAt(worldgenlevel, blockstate, blockpos, 2);
 			} else if (blockstate.getBlock() instanceof BambooBlock) {
-				placeBamboo(blockpos, worldgenlevel, pContext.random(), configuration);
+				placeBamboo(blockpos, worldgenlevel, randomsource);
 			} else {
 				if (Direction.stream().noneMatch((dir) -> worldgenlevel.isEmptyBlock(blockpos.relative(dir))))
 				{
@@ -61,29 +61,9 @@ public class SimpleBlockWithBambooFeature extends Feature<SimpleBlockWithBambooC
 	}
 
 	public void placeBamboo(BlockPos blockpos, WorldGenLevel worldgenlevel,
-	                           RandomSource randomsource, SimpleBlockWithBambooConfiguration configuration) {
+	                           RandomSource randomsource) {
 		BlockPos.MutableBlockPos blockpos$mutableblockpos = blockpos.mutable();
-		BlockPos.MutableBlockPos blockpos$mutableblockpos1 = blockpos.mutable();
 		int j = randomsource.nextInt(12) + 5;
-		//Podzol generation?
-		/*
-		if (randomsource.nextFloat() < configuration.bambooProbability()) {
-			int k = randomsource.nextInt(4) + 1;
-
-			for(int l = blockpos.getX() - k; l <= blockpos.getX() + k; ++l) {
-				for(int i1 = blockpos.getZ() - k; i1 <= blockpos.getZ() + k; ++i1) {
-					int j1 = l - blockpos.getX();
-					int k1 = i1 - blockpos.getZ();
-					if (j1 * j1 + k1 * k1 <= k * k) {
-						blockpos$mutableblockpos1.set(l, worldgenlevel.getHeight(Heightmap.Types.WORLD_SURFACE, l, i1) - 1, i1);
-						if (isDirt(worldgenlevel.getBlockState(blockpos$mutableblockpos1))) {
-							worldgenlevel.setBlock(blockpos$mutableblockpos1, Blocks.PODZOL.defaultBlockState(), 2);
-						}
-					}
-				}
-			}
-		}
-		*/
 		for(int l1 = 0; l1 < j && worldgenlevel.isEmptyBlock(blockpos$mutableblockpos); ++l1) {
 			worldgenlevel.setBlock(blockpos$mutableblockpos, BAMBOO_TRUNK, 2);
 			blockpos$mutableblockpos.move(Direction.UP, 1);
--- src/main/java/endurteam/inferior_accretion/world/level/levelgen/feature/configurations/SimpleBlockWithBambooConfiguration.java
+++ src/main/java/endurteam/inferior_accretion/world/level/levelgen/feature/configurations/SimpleBlockWithBambooConfiguration.java
@@ -5,12 +5,12 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
 
-public record SimpleBlockWithBambooConfiguration(float bambooProbability, BlockStateProvider toPlace) implements FeatureConfiguration
+public record SimpleBlockWithBambooConfiguration(float probability, BlockStateProvider toPlace) implements FeatureConfiguration
 {
 	public static final Codec<SimpleBlockWithBambooConfiguration> CODEC = RecordCodecBuilder.create((instance) ->
 			instance.group(
-					Codec.floatRange(0.0F, 1.0F).fieldOf("bambooProbability")
-							.forGetter((configuration) -> configuration.bambooProbability),
+					Codec.floatRange(0.0F, 1.0F).fieldOf("probability")
+							.forGetter((configuration) -> configuration.probability),
 					BlockStateProvider.CODEC.fieldOf("to_place")
 							.forGetter((configuration) -> configuration.toPlace))
 					.apply(instance, SimpleBlockWithBambooConfiguration::new));
